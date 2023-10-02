7c9657dbdfe5facccc9c2196fc072d42ca77644d
Henrry Beltrán <henrry26bt@gmail.com>
2023-10-02 14:48:12 -0500

    patch: remove cookie

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,17 +1,16 @@
-import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   const env = process.env.NODE_ENV;
 
-  cookies().set({
-    name: "my_cookie",
-    value: "jummy-cookie",
-    httpOnly: true,
-    domain: env === "development" ? ".localhost" : ".bunxt.vercel.app",
-    secure: env === "development" ? false : true,
-    path: "/",
-  });
+  // cookies().set({
+  //   name: "my_cookie",
+  //   value: "jummy-cookie",
+  //   httpOnly: true,
+  //   domain: env === "development" ? ".localhost" : ".bunxt.vercel.app",
+  //   secure: env === "development" ? false : true,
+  //   path: "/",
+  // });
 
   return NextResponse.json({ message: "Hello from NextJS API!", env });
 }
