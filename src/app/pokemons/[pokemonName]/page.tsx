6bccc8be0839b6a9c54c41ce19f8124e9e71d183
Henrry Beltrán <henrry26bt@gmail.com>
2023-10-02 11:56:31 -0500

    fix: Fix export default

--- src/app/pokemons/[pokemonName]/page.tsx
+++ src/app/pokemons/[pokemonName]/page.tsx
@@ -6,6 +6,21 @@ interface Props {
   };
 }
 
-const PokemonPage: FC<Props> = ({ params: { pokemonName } }) => {
-  return <></>;
+async function getPokemon(name: string): Promise<Pokemon> {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+
+  if (!res.ok) throw new Error("Couldn't fetch the data");
+
+  return res.json();
+}
+
+const PokemonPage: FC<Props> = async ({ params: { pokemonName } }) => {
+  const pokemon = await getPokemon(pokemonName);
+  return (
+    <>
+      <h1>{pokemon.name}</h1>
+    </>
+  );
 };
+
+export default PokemonPage;
